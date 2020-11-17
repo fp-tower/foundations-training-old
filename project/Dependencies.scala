be4ca60d7b9444285169dd8a171c81be7c6bf80b
Julien Truffaut <julien@fp-tower.com>
2020-11-17 20:31:54 +0000

    add setup instructions in README

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val catsEffect    = "org.typelevel"     %% "cats-effect"     % "2.2.0"
-  lazy val refined       = "eu.timepit"        %% "refined"         % "0.9.17"
+  lazy val refined       = "eu.timepit"        %% "refined"         % "0.9.18"
   lazy val circe         = "io.circe"          %% "circe-parser"    % "0.13.0"
   lazy val scalatest     = "org.scalatestplus" %% "scalacheck-1-14" % "3.1.2.0" % Test
   lazy val kindProjector = "org.typelevel"     %% "kind-projector"  % "0.10.3" cross CrossVersion.binary
